181c804629b80c5f9bb6517617e615369b3c87db
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-02 23:04:55 -0700

    Add files via upload

--- Analyzing_Repeat_Behavior.sql
+++ Analyzing_Repeat_Behavior.sql
@@ -3,21 +3,46 @@
 
 -- how many customers come back for another session from 2014 to nov 1 2014
 
--- rows for number of repeat sessions, and columns aggregate of the number of  sessions
+-- he wants rows for number of repeat sessions, and columns aggregate of the number of  sessions
 
--- multi step query, few ways to do it. First one to mind is make tables that says is repeat 1, is repeat 2, is repeat 3 etc
+-- first lets get the counts of each user ID
+
+-- then lets count the totals grouping by number of repeated sessions
+
+
 
 USE ecommerce_data;
 
-SELECT 
+CREATE TABLE repeat_table
 
-	is_repeat_session AS repeated_session,
-    COUNT( DISTINCT ) AS users
+SELECT
+
+	user_id AS user_id,
+    COUNT( user_id ) -1 AS repeats
 
 FROM website_sessions
 
-WHERE created_at BETWEEN '2014-01-01' AND '2014-11-01'
+WHERE created_at BETWEEN '2014-01-01 00:00:01' AND '2014-11-01'
 
-GROUP BY
+GROUP BY user_id
 
-ORDER BY
+
+ORDER BY user_id;
+
+
+SELECT 
+
+	repeat_table.repeats AS repeated_sessions,
+    COUNT( DISTINCT website_sessions.user_id ) AS users
+
+FROM website_sessions
+LEFT JOIN repeat_table
+	ON website_sessions.user_id = repeat_table.user_id
+
+WHERE website_sessions.created_at BETWEEN '2014-01-01 00:00:01' AND '2014-11-01'
+
+GROUP BY repeat_table.repeats
+
+ORDER BY repeat_table.repeats ASC;
+
+DROP TABLE repeat_table;
