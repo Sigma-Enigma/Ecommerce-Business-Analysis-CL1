6dc662aacd9b106fb31a99ba1f87db1bbf8a9810
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-03 18:52:35 -0700

    Add files via upload

--- Product_Level_Path_Analysis.sql
+++ Product_Level_Path_Analysis.sql
@@ -1,7 +1,7 @@
 -- Product Level Website Path Analysis
 
 -- select subset of website sessions that clicked to products (denominator of sessions)
--- for each session, find the minimum wepsite_pageview_id (or min created_at) where pageview_url = /products, print this value in column
+-- for each session, find the minimum website_pageview_id (or min created_at) where pageview_url = /products, print this value in column
 -- also for each session, find the 2nd 
 
 
@@ -10,7 +10,40 @@
 
 -- Finish later
 
+
+-- select only sessions that made it to product page (do this for both time windows)
+-- then find subset that made it to at least one other page after visting products page AND return the url of the next page this session_id visited (via 2nd min website pageview ID linked via website_session_id)
+
+-- then find subset that made it     "             "                 "              "   AND next url page returned == mr_fuzzy
+-- then find subset that made it     "             "                 "              "   AND next url page returned == love_bear
+
+
+-- eligible sessions
 SELECT
+	website_session_id,
+	MIN( CASE WHEN pageview_url = '/product' THEN website_pageview_id ELSE NULL END ) AS min_product_pageview_id,
+    
+   
+    
+    -- return url of min pageview_id
+    -- return 2nd min pageview_id
+    -- return url of 2nd min pageview_id
+    
+    
+FROM website_pageviews
+
+WHERE created_at BETWEEN '2013-01-05' AND '2013-04-06'
+
+GROUP BY website_session_id
+
+;
+
+
+ -- MIN( CASE WHEN website_pageview_id > ( MIN( CASE WHEN pageview_url = '/product' THEN website_pageview_id ELSE NULL END) ) THEN website_pageview_id ELSE NULL END) AS second_min_product_pageview_id, 
+
+
+SELECT
+	
 	COUNT( DISTINCT website_pageviews ) AS sessions,
 	COUNT( CASE WHEN THEN ELSE END) AS w_nextpage,
 	AS pct_nextpage,
