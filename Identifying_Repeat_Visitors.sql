a62a8b201b2425ce3992f68dbf1cecac725183b9
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-12 18:51:23 -0700

    Add files via upload

--- Identifying_Repeat_Visitors.sql
+++ Identifying_Repeat_Visitors.sql
@@ -6,6 +6,7 @@
 
 USE ecommerce_data;
 
+-- exploring datatable
 SELECT *
 
 FROM website_sessions
@@ -16,12 +17,33 @@ WHERE created_at BETWEEN '2014-01-01' AND '2014-11-01'
 -- first find count number of sessios per user ID within this time range
 SELECT 
 	user_id,
-	COUNT(website_session_id) AS sessions
+	COUNT(website_session_id) -1 AS sessions
     
 FROM website_sessions
 WHERE created_at BETWEEN '2014-01-01' AND '2014-11-01'
 GROUP BY 1
 ;
+
+CREATE TEMPORARY TABLE repeat_sessions
+SELECT 
+	user_id,
+	COUNT(website_session_id)-1 AS sessions
+    
+FROM website_sessions
+WHERE created_at BETWEEN '2014-01-01' AND '2014-11-01'
+GROUP BY 1
+;
+
+-- finally aggregate number of distinct users using number of repeat sessions as grouping value
+SELECT
+	repeat_sessions.sessions AS repeats,
+    COUNT( DISTINCT user_id) AS users
+FROM repeat_sessions
+
+GROUP BY 1
+;
+
+DROP TABLE repeat_sessions;
 
 
 -- next use prior table to count number of user_id's where sessions = 1, sessions = 2 etc. Grouping will be number of sessions-1 (number of repeat sessions)
