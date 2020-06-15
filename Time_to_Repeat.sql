c6f218cf780cd31e767d921900dd9739b4889cb0
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-14 23:26:57 -0700

    Add files via upload

---
+++ Time_to_Repeat.sql
@@ -0,0 +1,15 @@
+-- Analyzing Time to Repeat
+
+-- minimum maximum and average time to first and second sessions
+
+-- time frame: '2014-01-01' to '2014-11-04'
+
+USE ecommerce_data;
+
+SELECT
+
+FROM 
+
+WHERE
+;
+
