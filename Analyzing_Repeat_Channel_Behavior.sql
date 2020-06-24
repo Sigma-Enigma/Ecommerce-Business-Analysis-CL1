453244c3e26a675e10b8eb8f76a931c8cc6ee31e
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-23 22:39:39 -0700

    Add files via upload

--- Analyzing_Repeat_Channel_Behavior.sql
+++ Analyzing_Repeat_Channel_Behavior.sql
@@ -4,8 +4,56 @@
 USE ecommerce_data;
 
 SELECT
+user_id,
+website_session_id,
+created_at,
+is_repeat_session,
+utm_source,
+utm_campaign,
+http_referer,
+CASE 
+	WHEN (http_referer IS NULL) AND (utm_campaign IS NULL) THEN 'direct_type_in' 
+	WHEN (http_referer IS NOT NULL) AND (utm_campaign IS NULL) THEN 'organic_search'
+	WHEN (utm_campaign LIKE '%nonbrand%' )  THEN 'paid_nonbrand'
+	WHEN (utm_campaign LIKE '%brand%' )  THEN 'paid_brand'
+	ELSE 'paid_social' END AS channel_group
 
-FROM
+FROM website_sessions
 
-WHERE
+WHERE created_at BETWEEN '2014-01-01' AND '2014-11-05'
 ;
+
+CREATE TEMPORARY TABLE t1
+SELECT
+user_id,
+website_session_id,
+created_at,
+is_repeat_session,
+utm_source,
+utm_campaign,
+http_referer,
+CASE 
+	WHEN (http_referer IS NULL) AND (utm_campaign IS NULL) THEN 'direct_type_in' 
+	WHEN (http_referer IS NOT NULL) AND (utm_campaign IS NULL) THEN 'organic_search'
+	WHEN http_referer LIKE '%nonbrand%' THEN 'paid_nonbrand'
+	WHEN http_referer LIKE '%brand%'   THEN 'paid_brand'
+	ELSE 'paid_social' END AS channel_group
+
+FROM website_sessions
+
+WHERE created_at BETWEEN '2014-01-01' AND '2014-11-05'
+;
+
+-- null http_referer = direct_type_in
+-- null utm_campaign and non-null http_referer = organic_search
+-- create new column using case 
+
+
+SELECT
+	channel_group,
+	COUNT(website_session_id) AS counts
+FROM t1
+GROUP BY 1
+;
+
+DROP TABLE t1;
