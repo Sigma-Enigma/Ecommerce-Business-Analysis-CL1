557377643b5055719dceee7d2f155a1b891defc2
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-28 22:24:22 -0700

    Add files via upload

--- Analyzing_New_&_Repeat_Conversion_Rates.sql
+++ Analyzing_New_&_Repeat_Conversion_Rates.sql
@@ -6,3 +6,5 @@ SELECT *
 FROM website_sessions
 WHERE created_at BETWEEN '2014-01-01' AND '2014-11-08'
 ;
+
+-- columns is_repeat_session (either row group 0 or row group 1), sessions, conv_rate, rev_per_session
