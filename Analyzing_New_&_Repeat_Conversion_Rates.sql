817bec1faf16cda3efe088530fc02f03400a55e8
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-29 16:32:08 -0700

    Add files via upload

--- Analyzing_New_&_Repeat_Conversion_Rates.sql
+++ Analyzing_New_&_Repeat_Conversion_Rates.sql
@@ -2,9 +2,20 @@
 
 -- will need order data so join orders, also session data to determine number of potential buyers
 
-SELECT *
+SELECT
+	website_sessions.is_repeat_session,
+    COUNT(website_sessions.website_session_id) AS sessions,
+    COUNT(orders.order_id) / COUNT(website_sessions.website_session_id) AS conv_rate,
+    SUM(price_usd) / COUNT(website_sessions.website_session_id) AS rev_per_session
+    
 FROM website_sessions
-WHERE created_at BETWEEN '2014-01-01' AND '2014-11-08'
+	LEFT JOIN orders
+    ON orders.website_session_id = website_sessions.website_session_id
+    
+WHERE website_sessions.created_at BETWEEN '2014-01-01' AND '2014-11-08'
+
+GROUP BY
+website_sessions.is_repeat_session
 ;
 
 -- columns is_repeat_session (either row group 0 or row group 1), sessions, conv_rate, rev_per_session
