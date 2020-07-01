150df524f285d7feefccf386e2a81efff11d1eaa
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-06-30 23:14:51 -0700

    Add files via upload

---
+++ A Analysis Final.sql
@@ -0,0 +1,17 @@
+-- Analysis to Secure Second Funding Round 
+
+-- Objectives
+-- tell story of company growth using trended performance data
+-- Focus on ad channels and website optimization (how changes made the company more efficient and profitable)
+
+-- Specific Things to Address
+-- 1. Volume of growth. Overall session and order volume, trended by quarter. Including current incomplete quarter.
+-- 2. Efficiency of improvements. Conversion rates, revenue per order, and revenue per session, trended by quarter.
+-- 3. Channel growth. Orders grouped by ad channel, trended quarterly.
+-- 4. Channel specific conversion rates. Session to order conv rate, grouped by channel, trended by quarter. Note periods of large improvements.
+-- 5. Montly total revenue, total sales, and montly revenue and margin by product.
+-- 6. Montly sessions to products page. % of sessions that click to next page in funnel. Conversion rate from /products to placing order.
+-- 7. Cross sell analysis for products from '2014-12-05' to current date
+-- 8. Share recommendations for improvements the company can still make to further improve efficiency and profitability.
+
+-- Date is '2015-03-20'
