ef5f7ddbadd992d40149671e4e37999ff9a72cc9
Sigma-Enigma <32501315+Sigma-Enigma@users.noreply.github.com>
2020-07-01 16:13:22 -0700

    Add files via upload

--- A Analysis Final.sql
+++ A Analysis Final.sql
@@ -15,3 +15,36 @@
 -- 8. Share recommendations for improvements the company can still make to further improve efficiency and profitability.
 
 -- Date is '2015-03-20'
+
+USE ecommerce_data;
+
+SELECT
+	YEAR(website_sessions.created_at) AS yr,
+	QUARTER(website_sessions.created_at) AS qtr,
+    COUNT(website_sessions.website_session_id) AS sessions,
+    COUNT(CASE WHEN orders.order_id IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END  ) AS orders,
+    ROUND( 100*COUNT(CASE WHEN orders.order_id IS NOT NULL THEN website_sessions.website_session_id ELSE NULL END  ) / COUNT(website_sessions.website_session_id), 2) AS '% conv_rate',
+    ROUND( 100*SUM( orders.price_usd) / COUNT( orders.order_id), 2) AS rev_per_order,
+    ROUND( 100*SUM( orders.price_usd) / COUNT( website_sessions.website_session_id), 2) AS rev_per_session,
+    COUNT( CASE WHEN website_sessions.utm_source = 'gsearch' AND website_sessions.utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END ) AS gsearch_nonbrand_orders,
+    ROUND( 100*COUNT( CASE WHEN website_sessions.utm_source = 'gsearch' AND website_sessions.utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END ) / COUNT( CASE WHEN website_sessions.utm_source = 'gsearch' AND website_sessions.utm_campaign = 'nonbrand' THEN website_sessions.website_session_id ELSE NULL END ), 2) AS '% gsearch_nonbrand_conv',
+	COUNT( CASE WHEN website_sessions.utm_source = 'bsearch' AND website_sessions.utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END ) AS bsearch_nonbrand_orders,
+    ROUND( 100*COUNT( CASE WHEN website_sessions.utm_source = 'bsearch' AND website_sessions.utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END ) / COUNT( CASE WHEN website_sessions.utm_source = 'bsearch' AND website_sessions.utm_campaign = 'nonbrand' THEN website_sessions.website_session_id ELSE NULL END ), 2) AS '% bsearch_nonbrand_conv',
+	COUNT( CASE WHEN website_sessions.utm_campaign = 'brand' THEN orders.order_id ELSE NULL END ) AS all_brand_orders,
+    ROUND( 100*COUNT( CASE WHEN website_sessions.utm_campaign = 'brand' THEN orders.order_id ELSE NULL END ) / COUNT( CASE WHEN website_sessions.utm_campaign = 'brand' THEN website_sessions.website_session_id ELSE NULL END ), 2) AS '% all_brand_conv',
+    COUNT( CASE WHEN (website_sessions.utm_source IS NULL) AND (website_sessions.utm_campaign IS NULL) AND (http_referer IS NOT NULL) THEN orders.order_id ELSE NULL END ) AS organic_search_orders,
+    ROUND( 100*COUNT( CASE WHEN (website_sessions.utm_source IS NULL) AND (website_sessions.utm_campaign IS NULL) AND (http_referer IS NOT NULL) THEN orders.order_id ELSE NULL END ) / COUNT( CASE WHEN (website_sessions.utm_source IS NULL) AND (website_sessions.utm_campaign IS NULL) AND (http_referer IS NOT NULL) THEN website_sessions.website_session_id ELSE NULL END ), 2) AS '% organic_search_conv',
+    COUNT( CASE WHEN (website_sessions.utm_source IS NULL) AND (website_sessions.utm_campaign IS NULL) AND (http_referer IS NULL) THEN orders.order_id ELSE NULL END ) AS direct_type_in_orders,
+    ROUND( 100*COUNT( CASE WHEN (website_sessions.utm_source IS NULL) AND (website_sessions.utm_campaign IS NULL) AND (http_referer IS NULL) THEN orders.order_id ELSE NULL END ) / COUNT( CASE WHEN (website_sessions.utm_source IS NULL) AND (website_sessions.utm_campaign IS NULL) AND (http_referer IS NULL) THEN website_sessions.website_session_id ELSE NULL END ), 2) AS '% direct_type_in_conv'
+   
+    
+
+FROM website_sessions
+LEFT JOIN orders
+	ON website_sessions.website_session_id = orders.website_session_id
+
+WHERE website_sessions.created_at BETWEEN '2012-01-01'  AND '2015-03-20'
+
+GROUP BY 1,2
+;
+
